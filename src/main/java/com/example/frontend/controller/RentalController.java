7aba4033adea498613f719fc8bab996508c21487
Akanksha Chopra <chopraakanksha10@gmail.com>
2026-05-20 11:03:40 +0530

    rental changes

--- src/main/java/com/example/frontend/controller/RentalController.java
+++ src/main/java/com/example/frontend/controller/RentalController.java
@@ -2,15 +2,16 @@ package com.example.frontend.controller;
 
 import com.example.frontend.service.RentalService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 
+
+@Controller
+@RequestMapping("/rentals")
 public class RentalController {
     private final RentalService rentalService;
 
